5461558e90fe3810212c3ee7bfd8bb13c72729f7
Lars Larsson <lars.larsson@elastisys.com>
2021-04-27 12:36:27 +0200

    Renames DOMAIN to TOP_LEVEL_DOMAIN

--- cluster/configure_dns.sh
+++ cluster/configure_dns.sh
@@ -7,4 +7,4 @@ CLUSTER=${CLUSTER:-$(whoami)-demo-cluster}
 pushd kubespray
 INGRESS_CONTROLLER_LB_IP=$(terraform -chdir=contrib/terraform/exoscale/ output -raw ingress_controller_lb_ip_address)
 
-exo dns add A ${DOMAIN} -a ${INGRESS_CONTROLLER_LB_IP} -n "*.${CLUSTER}"
+exo dns add A ${TOP_LEVEL_DOMAIN} -a ${INGRESS_CONTROLLER_LB_IP} -n "*.${CLUSTER}"
