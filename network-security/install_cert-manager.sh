26631dd1cca32dc5f6d778f1b08efa27c14b773a
Lars Larsson <lars.larsson@elastisys.com>
2021-04-27 11:51:11 +0200

    Waits until cert-manager is installed

--- network-security/install_cert-manager.sh
+++ network-security/install_cert-manager.sh
@@ -2,13 +2,9 @@
 
 set -euo pipefail
 
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.crds.yaml
-
-sed -e "s/EMAIL_PLACEHOLDER/${EMAIL}/g" issuer.yml > rendered-issuer.yml
-
-kubectl apply -f rendered-issuer.yml
-
-kubectl create namespace certmanager
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
-helm install cert-manager jetstack/cert-manager
+helm upgrade cert-manager jetstack/cert-manager --namespace cert-manager --install --create-namespace --version v1.3.1 --set installCRDs=true --wait
+
+sed -e "s/EMAIL_PLACEHOLDER/${EMAIL}/g" issuer.yml > rendered-issuer.yml
+kubectl apply -f rendered-issuer.yml
