7c07086386a8780c9e567b761228a2c427c9a496
Lars Larsson <lars.larsson@elastisys.com>
2021-04-29 14:20:50 +0200

    Revert to using prod issuer

--- network-security/install_cert-manager.sh
+++ network-security/install_cert-manager.sh
@@ -6,5 +6,5 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm upgrade cert-manager jetstack/cert-manager --namespace cert-manager --install --create-namespace --version v1.3.1 --set installCRDs=true --wait
 
-sed -e "s/EMAIL_PLACEHOLDER/${EMAIL}/g" staging-issuer.yml > rendered-issuer.yml
+sed -e "s/EMAIL_PLACEHOLDER/${EMAIL}/g" prod-issuer.yml > rendered-issuer.yml
 kubectl apply -f rendered-issuer.yml
