195d945228d9b1cf463675d11bd920e62b998ffa
Lars Larsson <lars.larsson@elastisys.com>
2021-05-04 09:57:31 +0200

    Monitoring with Dex integration

---
+++ monitoring/install_monitoring.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CLUSTER=${CLUSTER:-$(whoami)-demo-cluster}
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+cp values.yaml rendered-values.yaml
+sed -i -e "s/DOMAIN_PLACEHOLDER/${CLUSTER}.${TOP_LEVEL_DOMAIN}/g" rendered-values.yaml
+
+helm upgrade --install --namespace=monitoring monitoring prometheus-community/kube-prometheus-stack --values=rendered-values.yaml --create-namespace
