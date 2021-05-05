4d168d01429a89fcdc04efe5391e1201e722e012
Lars Larsson <lars.larsson@elastisys.com>
2021-05-05 09:40:03 +0200

    ArgoCD with Dex OIDC SSO

---
+++ continuous-integration/install_argocd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CLUSTER=${CLUSTER:-$(whoami)-demo-cluster}
+
+cp argocd.yaml rendered-argocd.yaml
+sed -i -e "s/DOMAIN_PLACEHOLDER/${CLUSTER}.${TOP_LEVEL_DOMAIN}/g" rendered-argocd.yaml
+
+cp argocd-ingress.yaml rendered-argocd-ingress.yaml
+sed -i -e "s/DOMAIN_PLACEHOLDER/${CLUSTER}.${TOP_LEVEL_DOMAIN}/g" rendered-argocd-ingress.yaml
+
+if ! kubectl get namespace argocd; then
+				kubectl create namespace argocd
+fi
+kubectl apply --namespace argocd -f rendered-argocd.yaml
+kubectl apply --namespace argocd -f rendered-argocd-ingress.yaml
